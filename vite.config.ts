388873b1f732a5793caad7fdeab20c2afe56813b
Future-R <future.r@foxmail.com>
2026-01-04 02:48:33 +0800

    fix: github page 部署出问题

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Cloudflare Pages 部署建议使用绝对路径 '/'
-  base: '/', 
+  base: './', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
